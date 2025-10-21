7dc0b0b30edf5fa70a887051c862d14be3b3706e
nulisx <edrmebitch@fbi.one>
2025-10-21 19:04:54 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-  output: 'standalone',
-}
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+  images: { unoptimized: true },
 
-export default nextConfig
+  // === prevents huge webpack cache files ===
+  experimental: {
+    outputFileTracingRoot: __dirname,
+  },
+  output: 'standalone', // smaller deployable output
+  webpack: (config, { isServer }) => {
+    // prevents splitting giant chunks
+    config.optimization.splitChunks = {
+      chunks: 'all',
+      maxSize: 25000000, // 25 MB max per chunk
+    };
+    return config;
+  },
+};
+
+export default nextConfig;
